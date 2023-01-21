276e2c57adafc118941e7c4b581db5242803347d
Ebubekir Sezer <ebubekirssezer@gmail.com>
2023-01-22 00:39:07 +0300

    project created

---
+++ ActionOnSwift/ActionOnSwift/ActionOnSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionOnSwiftApp.swift
+//  ActionOnSwift
+//
+//  Created by EbubekirSezer on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ActionOnSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
